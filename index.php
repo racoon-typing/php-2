3a142bc904b96940a5959c418aae8260b4777c1e
Daniil <eo18622@yandex.ru>
2023-12-15 15:50:29 +0300

    комментирует

--- index.php
+++ index.php
@@ -7,5 +7,5 @@ ini_set('assets.exseption', 1);
 
 
 $strategy = new AvailableActions(AvailableActions::STATUS_NEW, 3, 1);
-assert($strategy->getNextStatus(AvailableActions::ACTION_CANCEL) == AvailableActions::STATUS_COMPLETE, 'cancel action');
+// assert($strategy->getNextStatus(AvailableActions::ACTION_CANCEL) == AvailableActions::STATUS_COMPLETE, 'cancel action');
 
