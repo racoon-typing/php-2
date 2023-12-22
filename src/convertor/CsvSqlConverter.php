0a61704d70c06b71d296194f7355153ac6e8a37c
Daniil <eo18622@yandex.ru>
2023-12-22 16:52:11 +0300

    getSql

--- src/convertor/CsvSqlConverter.php
+++ src/convertor/CsvSqlConverter.php
@@ -1,6 +1,7 @@
 <?php
 
-class CsvSqlConverter {
+class CsvSqlConverter
+{
     protected array $filesToConvert = [];
 
     public function __construct(string $directory)
@@ -12,5 +13,63 @@ class CsvSqlConverter {
         $this->loadCsvFiles($directory);
     }
 
-    // loadCsvFiles
+    public function convertFiles(string $outputDirectory)
+    {
+        $result = [];
+
+        foreach ($this->filesToConvert as $file) {
+            $result[] = $this->convertFile($file, $outputDirectory);
+        }
+
+        return $result;
+    }
+
+    protected function convertFile(SplFileInfo $file, string $outputDirectory)
+    {
+        $fileObject = new SplFileObject($file->getRealPath());
+        $fileObject->setFlags(SplFileObject::READ_CSV);
+
+        $columns = $fileObject->fgetcsv();
+        $values = [];
+
+        while (!$fileObject->eof()) {
+            $values[] = $fileObject->fgetcsv();
+        }
+
+        $tableName = $file->getBasename('.csv');
+        $sqlContent = $this->getSqlContent($tableName, $columns, $values);
+
+        return $this->saveSqlContent($tableName, $outputDirectory, $sqlContent);
+    }
+
+    protected function getSqlContent(string $tableName, array $columns, array $values): string
+    {
+        $columnsString = implode(', ', $columns);
+
+        $sql = "INSERT INTO $tableName ($columnsString) VALUES ";
+
+        foreach ($values as $row) {
+            array_walk($row, function (&$value) {
+                $value = addslashes($value);
+                $value = "'$value'";
+            });
+
+            $sql .= "( " . implode(', ', $row) . "), ";
+        }
+
+        $sql = substr($sql, 0, -2);
+
+        return $sql;
+    }
+
+
+
+    protected function loadCsvFiles(string $directory)
+    {
+        foreach (new DirectoryIterator($directory) as $file) {
+            if ($file->getExtension() == 'csv') {
+                $this->filesToConvert[] = $file->getFileInfo();
+            }
+        }
+    }
 }
