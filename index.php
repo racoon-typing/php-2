7dd39cac1c20df2648b762c76fd61e53ce3fcfc7
Daniil <eo18622@yandex.ru>
2023-12-22 11:57:03 +0300

    задание 3.14 csvReader

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+use taskforce\csv\CsvReader;
+use taskforce\exception\FileFormatException;
+use taskforce\exception\SourceFileException;
+
 use taskforce\logic\AvailableActions;
 use taskforce\logic\exception\StatusActionException;
 
@@ -7,16 +11,31 @@ require_once 'vendor/autoload.php';
 ini_set('assets.exseption', 1);
 
 
-try {
-    $strategy = new AvailableActions(AvailableActions::STATUS_NEW, 3, 1);
+// try {
+//     $strategy = new AvailableActions(AvailableActions::STATUS_NEW, 3, 1);
 
-    $strategy->setStatus(AvailableActions::STATUS_CANCEL);
-    $strategy->getAvailableActions('performer2', 2);
-} catch (StatusActionException $e) {
-    $errorMessage = 'Ошибка при установке статуса: ' . $e->getMessage();
-    echo $errorMessage;
-    error_log($errorMessage, 3, 'error.log');
-} 
+//     $strategy->setStatus(AvailableActions::STATUS_CANCEL);
+//     $strategy->getAvailableActions('performer2', 2);
+// } catch (StatusActionException $e) {
+//     $errorMessage = 'Ошибка при установке статуса: ' . $e->getMessage();
+//     echo $errorMessage;
+//     error_log($errorMessage, 3, 'error.log');
+// } 
 
 // $newToPerformer = $strategy->getAvailableActions(AvailableActions::ROLE_PERFORMER, 2);
 // var_dump('new -> performer', !empty($newToPerformer));
+
+
+$loader = new CsvReader('data/cities.csv', ['name', 'lat', 'long']);
+$result = [];
+
+try {
+    $loader->import();
+    $result = $loader->getData();
+} catch (SourceFileException $e) {
+    error_log("Не удалось обработать csv файл: " . $e->getMessage());
+} catch (FileFormatException $e) {
+    error_log("Неверная форма файла импорта: " . $e->getMessage());
+}
+
+var_dump($result);
---
+++ src/csv/CsvReader.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace taskforce\csv;
+
+use SplFileObject;
+use taskforce\exception\FileFormatException;
+use taskforce\exception\SourceFileException;
+
+class CsvReader
+{
+    private string $filename;
+    private array $columns;
+    private $file;
+
+    private array $result = [];
+
+
+    /**
+     * ContactsImporter constructor.
+     * @param $filename
+     * @param $columns
+     */
+    public function __construct(string $filename, array $columns)
+    {
+        $this->filename = $filename;
+        $this->columns = $columns;
+    }
+
+    public function import(): void
+    {
+        if (!$this->validateColumns($this->columns)) {
+            throw new FileFormatException("Заданы неверные заголовки столбцов");
+        }
+        
+        if (!file_exists($this->filename)) {
+            throw new SourceFileException("Файл не существует");
+        }
+        
+        $this->file = new SplFileObject($this->filename, 'r');
+
+        if (!$this->file) {
+            throw new SourceFileException("Не удалось открыть файл на чтение");
+        }
+
+        $header_data = $this->getHeaderData();
+
+        if ($header_data !== $this->columns) {
+            throw new FileFormatException("Исходный файл не содержит необходимых столбцов");
+        }
+
+        while ($line = $this->getNextLine()) {
+            $this->result[] = $line;
+        }
+    }
+
+    public function getData(): array
+    {
+        return $this->result;
+    }
+
+    private function getHeaderData(): ?array
+    {
+        $this->file->seek(0);
+        $data = $this->file->fgetcsv();
+
+        return $data;
+    }
+
+    private function getNextLine(): ?array
+    {
+        $result = null;
+
+        if (!$this->file->eof()) {
+            $result = $this->file->fgetcsv();
+        }
+
+        return $result;
+    }
+
+    private function validateColumns(array $columns): bool
+    {
+        $result = true;
+
+        if (count($columns)) {
+            foreach($columns as $column) {
+                if (!is_string($column)) {
+                    $result = false;
+                }
+            }
+        }
+        else {
+            $result = false;
+        }
+
+        return $result;
+    }
+}
