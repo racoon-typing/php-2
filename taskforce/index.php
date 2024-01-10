dd34fc8b1509cb0a1519c4b4b381836b0e5e4e78
Daniil <eo18622@yandex.ru>
2024-01-10 15:40:37 +0300

    задание 4.7 готово

--- taskforce/index.php
+++ taskforce/index.php
@@ -11,8 +11,6 @@ $result = $converter->convertFiles('data/sql');
 
 var_dump($result);
 
-
-
 // use taskforce\logic\AvailableActions;
 // use taskforce\exception\StatusActionException;
 
@@ -29,21 +27,3 @@ var_dump($result);
 
 // $newToPerformer = $strategy->getAvailableActions(AvailableActions::ROLE_PERFORMER, 2);
 // var_dump('new -> performer', !empty($newToPerformer));
-
-
-
-
-
-// $loader = new CsvReader('./data/cities.csv', ['name', 'lat']);
-// $result = [];
-
-// try {
-//     $loader->import();
-//     $result = $loader->getData();
-// } catch (SourceFileException $e) {
-//     error_log("Не удалось обработать csv файл: " . $e->getMessage());
-// } catch (FileFormatException $e) {
-//     error_log("Неверная форма файла импорта: " . $e->getMessage());
-// }
-
-// var_dump($result);
--- taskforce/views/layouts/main.php
+++ taskforce/views/layouts/main.php
@@ -4,11 +4,6 @@
 /** @var string $content */
 
 use app\assets\AppAsset;
-use app\widgets\Alert;
-use yii\bootstrap5\Breadcrumbs;
-use yii\bootstrap5\Html;
-use yii\bootstrap5\Nav;
-use yii\bootstrap5\NavBar;
 
 AppAsset::register($this);
 
@@ -19,65 +14,74 @@ $this->registerMetaTag(['name' => 'description', 'content' => $this->params['met
 $this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_keywords'] ?? '']);
 $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii::getAlias('@web/favicon.ico')]);
 ?>
+
 <?php $this->beginPage() ?>
+
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>" class="h-100">
+<html lang="ru">
+
 <head>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Taskforce</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body class="d-flex flex-column h-100">
-<?php $this->beginBody() ?>
 
-<header id="header">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest
-                ? ['label' => 'Login', 'url' => ['/site/login']]
-                : '<li class="nav-item">'
-                    . Html::beginForm(['/site/logout'])
-                    . Html::submitButton(
-                        'Logout (' . Yii::$app->user->identity->username . ')',
-                        ['class' => 'nav-link btn btn-link logout']
-                    )
-                    . Html::endForm()
-                    . '</li>'
-        ]
-    ]);
-    NavBar::end();
-    ?>
-</header>
+<body>
+    <?php $this->beginBody() ?>
 
-<main id="main" class="flex-shrink-0" role="main">
-    <div class="container">
-        <?php if (!empty($this->params['breadcrumbs'])): ?>
-            <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
-        <?php endif ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</main>
-
-<footer id="footer" class="mt-auto py-3 bg-light">
-    <div class="container">
-        <div class="row text-muted">
-            <div class="col-md-6 text-center text-md-start">&copy; My Company <?= date('Y') ?></div>
-            <div class="col-md-6 text-center text-md-end"><?= Yii::powered() ?></div>
+    <header class="page-header">
+        <nav class="main-nav">
+            <a href='#' class="header-logo">
+                <img class="logo-image" src="img/logotype.png" width=227 height=60 alt="taskforce">
+            </a>
+            <div class="nav-wrapper">
+                <ul class="nav-list">
+                    <li class="list-item list-item--active">
+                        <a class="link link--nav">Новое</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="#" class="link link--nav">Мои задания</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="#" class="link link--nav">Создать задание</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="#" class="link link--nav">Настройки</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="user-block">
+            <a href="#">
+                <img class="user-photo" src="img/man-glasses.png" width="55" height="55" alt="Аватар">
+            </a>
+            <div class="user-menu">
+                <p class="user-name">Василий</p>
+                <div class="popup-head">
+                    <ul class="popup-menu">
+                        <li class="menu-item">
+                            <a href="#" class="link">Настройки</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#" class="link">Связаться с нами</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#" class="link">Выход из системы</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
-</footer>
+    </header>
 
-<?php $this->endBody() ?>
+    <main class="main-content main-content--center container">
+        <!-- $content -->
+        <h1>Hello world!</h1>
+    </main>
+
+    <?php $this->endBody() ?>
 </body>
+
 </html>
 <?php $this->endPage() ?>
