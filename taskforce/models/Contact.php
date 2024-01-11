1bf70dc29534378a29d313f45155a9e2e00e7dfa
Daniil <eo18622@yandex.ru>
2024-01-11 13:22:05 +0300

    добавит контакты в модель

---
+++ taskforce/models/Contact.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+
+class Contact extends Model
+{
+    public $name;
+    public $phone;
+    public $email;
+    public $position;
+
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Имя',
+            'phone' => 'Телефон',
+            'email' => 'Электронная почта',
+            'position' => 'Должность'
+        ];
+    }
+}
